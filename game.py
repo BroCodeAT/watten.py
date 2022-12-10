6f6dab3767768a5541941961618eaa0016a9ac72
Daniel Pfurner <dpfurner@tsn.at>
2022-12-10 23:52:29 +0100

    display the player_name in a different line that chri understands it

--- game.py
+++ game.py
@@ -15,8 +15,9 @@ while playing:
     for cards in [3, 2]:
         for client in server.clients:
             deal_cards = card_dek.deal_top_card(cards)
+            player_name = server.clients[client]["name"]
             try:
-                player[server.clients[client]["name"]]["cards"].append(deal_cards)
+                player[player_name]["cards"].append(deal_cards)
             except KeyError:
-                player[server.clients[client]["name"]] = {"cards": deal_cards}
+                player[player_name] = {"cards": deal_cards}
             server.send_to("NEW_CARD", client, cards=deal_cards)
