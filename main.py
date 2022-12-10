5ad769f0194f02e8b7aa7188a41f5fd361b36961
Daniel Pfurner <dpfurner@tsn.at>
2022-12-10 22:13:05 +0100

    Custom Text Input Window

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import pygame
-import pygame_textinput
 from client import NetworkClient
 import utils
 
@@ -15,23 +14,19 @@ pygame.display.set_caption("Watten")
 # Pygame now allows natively to enable key repeat:
 pygame.key.set_repeat(200, 25)
 
-username_input = utils.UsernameInputField()
 
 while True:
-    game_display.blit(background, (0,0))
+    game_display.blit(background, (0, 0))
     events = pygame.event.get()
 
     if not username:
-        username_input.show(display=game_display, events=events)
+        username = utils.text_input(game_display, clock)
+        client.server_connect(username)
 
     for event in events:
         if event.type == pygame.QUIT:
             pygame.quit()
             break
-
-        elif username_input and event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
-            username = username_input.input_field.value
-            username_input = None
 
     pygame.display.update()
     clock.tick(60)
--- utils.py
+++ utils.py
@@ -1,19 +1,60 @@
 import pygame
-import pygame_textinput 
 from typing import List
 
-class UsernameInputField():
-    def __init__(self) -> None:
-        manager = pygame_textinput.TextInputManager(validator = lambda input: len(input) <= 13)
-        self.input_field = pygame_textinput.TextInputVisualizer(manager=manager)
 
-        self.input_field.cursor_blink_interval = 400
-        self.input_field.value = "Username"
-        
-        
+def draw_text(text, color, surface, x, y, text_size=30):
+    font = pygame.font.SysFont(None, text_size)
+    obj = font.render(text, True, color)
+    rect = obj.get_rect()
+    rect.topleft = (x, y)
+    surface.blit(obj, rect)
 
-    def show(self, display: pygame.Surface, events: List[pygame.event.Event]):
-        self.input_field.update(events)
-        pygame.draw.rect(display, (255, 255, 255), pygame.Rect(335,330,330,40))
-        display.blit(self.input_field.surface, (345,340))
+
+def text_input(screen: pygame.Surface, clock: pygame.time.Clock):
+    click = False
+    run = True
+    comp_name = ""
+    while run:
+        pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(200, 200, 600, 300), 0, 5)
+
+        draw_text("Welcome to Watten", (12, 255, 255), screen, 220, 220, 50)
+        draw_text("Created by kloaChristoph/GozZzer", (12, 255, 255), screen, 220, 260, 20)
+        draw_text("Insert your Username and press Start", (12, 255, 255), screen, 220, 310, 35)
+
+        name_box = pygame.Rect(220, 350, 460, 60)
+        pygame.draw.rect(screen, (0, 0, 0), name_box, 0, 5)
+        input = pygame.font.Font(None, 50).render(comp_name, True, (255, 255, 255))
+        screen.blit(input, input.get_rect(center=name_box.center))
+
+        start_button = pygame.Rect(220, 420, 130, 60)
+
+        mx, my = pygame.mouse.get_pos()
+        if start_button.collidepoint((mx, my)):
+            if click:
+                if comp_name:
+                    return comp_name
+
+        pygame.draw.rect(screen, (0, 161, 255), start_button, 0, 4)
+        draw_text("START", (12, 255, 255), screen, 230, 435, 50)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                click = True
+            if event.type == pygame.MOUSEBUTTONUP:
+                click = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+                elif event.key == pygame.K_BACKSPACE:
+                    comp_name = comp_name[:-1]
+                else:
+                    if len(comp_name) < 14:
+                        comp_name += event.unicode
+
+        pygame.display.update()
+        clock.tick(60)
         
