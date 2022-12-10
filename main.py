6417a010f59b1464e43dd5d6dadd26d0927dbd07
kloaChristoph <christoph.alexander.holas@gmail.com>
2022-12-10 20:45:17 +0100

    added background to game

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ from client import NetworkClient
 client = NetworkClient()
 
 pygame.init()
+background = pygame.image.load(r"cards\background.png")
 
-pygame.display.set_mode((1000, 700))
+game_display = pygame.display.set_mode((1000, 700))
 pygame.display.set_caption("Watten")
 
 while True:
+    game_display.blit(background, (0,0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
