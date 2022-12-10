6c8a24223ce5f606ba13b5d916596c4bf02b9ddd
kloaChristoph <christoph.alexander.holas@gmail.com>
2022-12-10 20:00:49 +0100

    WIP: client

--- client.py
+++ client.py
@@ -2,6 +2,7 @@ import socket
 
 HOST = "127.0.0.1"
 PORT = 3333
+FORMAT = "UTF-8"
 
 class NetworkClient(socket.socket):
     def __init__(self):
@@ -11,8 +12,15 @@ class NetworkClient(socket.socket):
         self.connect((host, port))
         self.send(name.encode())
 
+    def send(self,msg: str):
+        to_send = msg.encode(FORMAT)
+        self.send(to_send)
+
+
 
 if __name__ == '__main__':
     client = NetworkClient()
     username = input("Please enter your name: ")
     client.server_connect(username, host=HOST, port=PORT)
+    
+    client.send("test 1234")
