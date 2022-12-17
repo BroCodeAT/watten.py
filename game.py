9aabdd763c6766a19ed012952e5c5e3f4e109a9f
kloaChristoph <christoph.alexander.holas@gmail.com>
2022-12-17 13:55:21 +0100

    sending 5 cards instead of 3 and 2

--- game.py
+++ game.py
@@ -13,6 +13,7 @@ playing = True
 
 while playing:
     card_dek = CardDek.get_mixed_dek()
+    #dealing cards
     for cards in [3, 2]:
         for client in server.clients:
             deal_cards = card_dek.deal_top_card(cards)
@@ -21,7 +22,12 @@ while playing:
                 player[player_name]["cards"].extend(deal_cards)
             except KeyError:
                 player[player_name] = {"cards": deal_cards}
-            server.send_to("NEW_CARD", client, cards=list(map(int, deal_cards)))
+
+    #sending cards to clients
+    for client in server.clients:
+        player_name = server.clients[client]["name"]
+        cards_to_send = player[player_name]["cards"]
+        server.send_to("NEW_CARD", client, cards=list(map(int, cards_to_send)))
 
     server.send_all("SO_LOS_GEHTS")
 
