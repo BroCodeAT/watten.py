5a5eec72633a05d9e74a1abc1a2a3b635bbaa3c7
Daniel Pfurner <dpfurner@tsn.at>
2022-12-18 23:13:16 +0100

    New Server Side Command: PLAYER_NAMES to send the names of the players to the Clients

--- Commands.md
+++ Commands.md
@@ -10,4 +10,11 @@
 - to: str → Name of the Client to send the Command to
 - cards: list[int] → The ID of the Cards you get
 
+### PLAYER_NAMES
+    The Client receives the Names of the other Clients (Client is first)
+
+**Attributes:**
+- to: str → Name of the Client to send the Command to
+- players: list[str] → The List of the Names of the Players (to is first)
+
 # Client to Server
--- game.py
+++ game.py
@@ -8,11 +8,13 @@ server.accept_clients()
 
 card_dek: CardDek | None = None
 player: dict[str, dict[str, list]] = {}
+cur_player: str | None = None
 
 playing = True
 
 while playing:
     card_dek = CardDek.get_mixed_dek()
+
     # dealing cards
     for cards in [3, 2]:
         for client in server.clients:
@@ -23,10 +25,18 @@ while playing:
             except KeyError:
                 player[player_name] = {"cards": deal_cards}
 
+    # set the name of the starting player as cur_player
+    if cur_player:
+        cur_player = list(player)[list(player).index(cur_player) + 1]
+    else:
+        cur_player = list(player)[0]
+
     # sending cards to clients
     for client in server.clients:
         player_name = server.clients[client]["name"]
         cards_to_send = player[player_name]["cards"]
+        server.send_to("PLAYER_NAMES", client, players=list(player)[list(player).index(player_name):] + list(player)[:list(player).index(player_name)])
+
         server.send_to("NEW_CARD", client, cards=list(map(int, cards_to_send)))
 
     server.send_all("SO_LOS_GEHTS")
