56b439c5cf52ae8e2792349594b488a1cc67825f
Daniel Pfurner <dpfurner@tsn.at>
2022-12-29 19:04:18 +0100

    WIP: Documentation (NumPy) draw_text, text_input, load_card_image

--- client/client_utils.py
+++ client/client_utils.py
@@ -1,17 +1,55 @@
 import sys
 import pygame
-from typing import List
 
 
-def draw_text(text, color, surface: pygame.Surface, x, y, text_size=30):
+def draw_text(text: str, color: tuple[int, int, int], surface: pygame.Surface, x: int, y: int, text_size: int = 30) -> pygame.rect.Rect:
+    """
+    Draw text with the given color on the given surface to the given coordination's
+
+    Parameters
+    ----------
+    text : str
+        The text to display
+    color : tuple[int, int, int]
+        The RGB code of the color of the text
+    surface : pygame.Surface
+        The surface on witch the text should be drawn
+    x : int
+        The x-coordinate the text should be drawn to
+    y : int
+        The y-coordinate the text should be drawn to
+    text_size : int (default: 30)
+        The size of the text
+
+    Returns
+    -------
+    pygame.rect.Rect : The Rectangular of the text
+    """
     font = pygame.font.SysFont(None, text_size)
     obj = font.render(text, True, color)
     rect = obj.get_rect()
     rect.topleft = (x, y)
     surface.blit(obj, rect)
+    return rect
 
 
-def text_input(screen: pygame.Surface, clock: pygame.time.Clock):
+def text_input(screen: pygame.Surface, clock: pygame.time.Clock, length: int = 10) -> str:
+    """
+    Screen to enter a text input from the client
+
+    Parameters
+    ----------
+    screen : pygame.Surface
+        The screen where the text input should be placed on
+    clock : pygame.time.Clock
+        The clock, the game is synced to
+    length : int (default: 10)
+        The length of the text input
+
+    Returns
+    -------
+    str : the text the user entered
+    """
     click = False
     run = True
     username = ""
@@ -56,7 +94,7 @@ def text_input(screen: pygame.Surface, clock: pygame.time.Clock):
                     if username:
                         return username
                 else:
-                    if len(username) < 10:
+                    if len(username) < length:
                         username += event.unicode
 
         pygame.display.update()
@@ -64,13 +102,27 @@ def text_input(screen: pygame.Surface, clock: pygame.time.Clock):
 
 
 def load_card_image(player_names: list[str], card_ids: list[int]) -> dict[str, list[pygame.Surface]]:
+    """
+    Load the cards to the screen
+
+    Parameters
+    ----------
+    player_names : list[str]
+        The list of the names of every player
+    card_ids : list[int]
+        The list of the cards of the player
+
+    Returns
+    -------
+    dict[str, list[pygame.Surface]]: The name associated to a list of the surfaces of the cards
+    """
     own_cards: list = []
     for card_id in card_ids:
-        own_card = pygame.image.load(fr"cards/id_{card_id}.png")
+        own_card = pygame.image.load(fr"client/cards/id_{card_id}.png")
         own_card = pygame.transform.scale(own_card, (100, 170))
         own_cards.append(own_card)
 
-    card_back = pygame.image.load(r"cards/card_back.png")
+    card_back = pygame.image.load(r"client/cards/card_back.png")
     card_back = pygame.transform.scale(card_back, (100, 170))
     card_back_rot = pygame.transform.rotate(card_back, 90)
 
