fc77ee56ba9c812f56bb8b3a7b07513e01fa4339
Daniel Pfurner <dpfurner@tsn.at>
2022-12-29 18:27:34 +0100

    WIP: Documentation (NumPy)/Conversion of ClientGameData to a dataclass

--- client/client_models.py
+++ client/client_models.py
@@ -1,13 +1,32 @@
 import pygame
+from dataclasses import dataclass, field
 
 
+@dataclass
 class ClientGameData:
-    def __init__(self):
-        self.username: str = ""
-        self.player_names: list[str] = []
+    """
+    A dataclass to represent a Client including all the needed client Data.
 
-        self.card_ids: list[int] = []
-        self.highlighted_pos: list[int] = []
+    ...
 
-        self.game_display = pygame.display.set_mode((1000, 700))
-        self.player_cards_surfaces = {}
+    Attributes
+    ----------
+    username: str = ""
+        The name of the client
+    player_names: list[str] (default: list)
+        The names of all the clients
+    card_ids: list[int] (default: list)
+        A list of the cards the player currently has
+    highlighted_pos: list[int] (default: list)
+        The positions of the highlighted cards
+    game_display: pygame.Surface
+        The whole game surface
+    player_card_surfaces: dict (default: dict)
+        The surfaces of the cards of the players
+    """
+    username: str = ""
+    player_names: list[str] = field(default_factory=list)
+    card_ids: list[int] = field(default_factory=list)
+    highlighted_pos: list[int] = field(default_factory=list)
+    game_display: pygame.Surface = pygame.display.set_mode((1000, 700))
+    player_card_surfaces: dict = field(default_factory=dict)
