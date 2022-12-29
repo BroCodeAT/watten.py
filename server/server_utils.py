3d0e507920b88f4b0d8f8dcb029afaffae5c68d4
Daniel Pfurner <dpfurner@tsn.at>
2022-12-29 13:35:00 +0100

    When no card is played every card will be highlighted

--- server/server_utils.py
+++ server/server_utils.py
@@ -1,9 +1,11 @@
 from models import CardBase
 from typing import List
 
+
 def check_available(player_cards: List[CardBase], already_played: List[CardBase], highest: CardBase, know: bool = True):
-    if already_played[0] == highest:
-        playable = [card for card in player_cards if card == highest]
-        if playable:
-            return playable
+    if already_played:
+        if already_played[0] == highest:
+            playable = [card for card in player_cards if card == highest]
+            if playable:
+                return playable
     return player_cards
